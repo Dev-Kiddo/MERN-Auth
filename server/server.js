03dea8379e4202f845d91fb16ebf38c9928cf75e
Dev-Kiddo <buzzwithprasanth@gmail.com>
2025-10-07 20:08:10 +0530

    database config

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -10,7 +11,17 @@ app.use(express.json());
 app.use(cors({ credentials: true }));
 app.use(cookieParser());
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("✅ MongoDB connected successfully");
+  } catch (error) {
+    console.error("❌ Mongo Failed:", error);
+    process.exit(1);
+  }
+};
+
 app.listen(port, () => {
   console.log(`Server is connected at PORT: ${port}`);
-  console.log("hello");
+  connectDB();
 });
