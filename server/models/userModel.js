a914108e26084dd365d89329779ce810c7445e21
Dev-Kiddo <buzzwithprasanth@gmail.com>
2025-10-07 20:31:50 +0530

    userSchema and model created

---
+++ server/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
